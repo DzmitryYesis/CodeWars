a26bfadcf0a29fe644d582c169a619135b556f66
DzmitryYesis <yesis1988@mail.ru>
2021-10-25 17:30:08 +0300

    created wars13

---
+++ CodeWars13.js
@@ -0,0 +1,8 @@
+function arrayPlusArray(arr1, arr2) {
+    var sumArray=arr1.concat(arr2);
+    var sum = 0;
+  for(var i = 0; i < sumArray.length; i++){
+      sum += sumArray[i];
+      }
+    return sum; //something went wrong
+  }
