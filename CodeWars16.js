63bb6a2d1ceb2df1c34fcb66b58bfbdda47c58a7
DzmitryYesis <yesis1988@mail.ru>
2021-10-25 23:11:33 +0300

    created wars16

---
+++ CodeWars16.js
@@ -0,0 +1,9 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    var raznica=dadYearsOld-sonYearsOld*2;
+    if (raznica>=0) {
+      return raznica;
+    } else {
+      return raznica*=-1;
+    }
+    // your code here
+  }
