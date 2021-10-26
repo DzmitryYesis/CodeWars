f7c53ccd2e47267f6b8e715a4ebb5399eb5b1f3e
DzmitryYesis <yesis1988@mail.ru>
2021-10-26 21:06:00 +0300

    created wars36

---
+++ CodeWars36.js
@@ -0,0 +1,37 @@
+function switchItUp(number){
+    var str='';
+    switch(number) {
+        case 1:
+        str+="One";
+        break;
+        case 2:
+        str+="Two";
+        break;
+        case 3:
+        str+="Three";
+        break;
+        case 4:
+        str+="Four";
+        break;
+        case 5:
+        str+="Five";
+        break;
+        case 6:
+        str+="Six";
+        break;
+        case 7:
+        str+="Seven";
+        break;
+        case 8:
+        str+="Eight";
+        break;
+        case 9:
+        str+="Nine";
+        break;
+        case 0:
+        str+="Zero";
+        
+    }
+    return str;
+  //Write your own Code!
+  }
