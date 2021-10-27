c0016f6f1458f8d4defcff209d9da76ae7c970fc
DzmitryYesis <yesis1988@mail.ru>
2021-10-27 18:39:29 +0300

    created wars60

---
+++ CodeWars60.js
@@ -0,0 +1,25 @@
+const quarterOf = (month) => {
+    var sum=0;
+    switch(month) {
+        case 1:
+        case 2:
+        case 3:
+        sum+=1;
+        break;
+        case 4:
+        case 5:
+        case 6:
+        sum+=2;
+        break;
+        case 7:
+        case 8:
+        case 9:
+        sum+=3;
+        break;
+        case 10:
+        case 11:
+        case 12:
+        sum+=4;
+    }
+    return sum
+    // Your code here
