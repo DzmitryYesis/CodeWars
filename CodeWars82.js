76bb2aaf6bd50df415679a3e53e234e7bb3bae52
DzmitryYesis <yesis1988@mail.ru>
2021-10-28 19:34:58 +0300

    created wars82

---
+++ CodeWars82.js
@@ -0,0 +1,11 @@
+function between(a, b) {
+    var myArray = [];
+    var i = a;
+    while (i<=b) {
+      myArray.push(i);
+      i++;
+    }
+    return myArray;
+      
+      // your code here
+    }
