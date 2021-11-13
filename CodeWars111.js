48f8c542903f737e4e7bfba5e191f20bc0010caf
DzmitryYesis <yesis1988@mail.ru>
2021-11-13 14:14:55 +0300

    created wars111

---
+++ CodeWars111.js
@@ -0,0 +1,15 @@
+// Sum Numbers
+function sum (numbers) {
+    let sum1=0;
+    if(numbers.length<1) {
+        return 0
+    } else {
+        for(let i=0; i<numbers.length; i++){
+            sum1+=numbers[i];
+        }
+        return sum1;
+    }
+    "use strict";
+
+
+};
