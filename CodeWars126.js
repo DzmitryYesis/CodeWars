8403b3e158d00bbed1904c8eca75f2b582f77087
DzmitryYesis <yesis1988@mail.ru>
2021-12-01 21:54:16 +0300

    'created wars126'

---
+++ CodeWars126.js
@@ -0,0 +1,6 @@
+var rooms = {
+    name:{name:'Zal', description:'Beautiful', completed:true},
+    description:{name:'Spalnia', description:'Empty', completed:true},
+    completed:{name:'Kithen', description:'Crazy', completed:false}
+
+}
