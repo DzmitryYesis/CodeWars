167a58246021fb93069d017e9cebcad8e3c29f6e
DzmitryYesis <yesis1988@mail.ru>
2022-06-08 15:06:48 +0300

    'created wars133'

---
+++ CodeWars133.js
@@ -0,0 +1,8 @@
+function chromosomeCheck(sperm) {
+    switch (sperm) {
+        case 'XY':
+            return "Congratulations! You're going to have a son."
+        case 'XX':
+            return "Congratulations! You're going to have a daughter."
+    }
+}
