8241bc965399fb837602b0758231bef47c6d0c77
DzmitryYesis <yesis1988@mail.ru>
2022-06-09 11:16:04 +0300

    'created wars138'

---
+++ CodeWars138.js
@@ -0,0 +1,18 @@
+function howManydays(month) {
+    var days;
+    switch (month) {
+        case 2:
+            return days = 28
+        case 4:
+            return days = 30
+        case 6:
+            return days = 30
+        case 9:
+            return days = 30
+        case 11:
+            return days = 30
+        default:
+            return days = 31
+    }
+    return days;
+}
