2e8e2835fbbcdbc800d5a9e6f6650a81879f10b7
DzmitryYesis <yesis1988@mail.ru>
2022-06-14 19:45:26 +0300

    'created wars151'

---
+++ CodeWars151.js
@@ -0,0 +1,8 @@
+function bigToSmall(arr) {
+    let res = []
+    for (let i = 0; i < arr.length; i++) {
+        res = res.concat(arr[i])
+    }
+    return res.sort((a, b) => a - b).reverse().join('>')
+
+}
